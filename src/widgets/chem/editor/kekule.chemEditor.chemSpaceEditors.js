a0a31c11820310b0e6dd6bbdd41357b4a0cb6b0f
Nathan Hayden <nathanhayden@gmail.com>
2018-07-20 09:50:29 -0500

    fix matching when charges are added and removed (#9)
    (cherry picked from commit 71ec4283732e15b6656af22efe47ccd50c161da4)

--- src/widgets/chem/editor/kekule.chemEditor.chemSpaceEditors.js
+++ src/widgets/chem/editor/kekule.chemEditor.chemSpaceEditors.js
@@ -6191,6 +6191,10 @@ Kekule.Editor.MolNodeChargeIaController = Class.create(Kekule.Editor.AttachedMar
 			modifiedData.radical = this.getRadical();
 			radicalModified = true;
 		}
+		if (modifiedData.charge === 0) {
+			modifiedData.radical = undefined;
+			radicalModified = true;
+		}
 
 		var result = [];
 		// add or remove marker operation
