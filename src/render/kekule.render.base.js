d8e4ea6b7f0e70366968e3c0dbababb6325d73c7
Alejandro Riccillo <ariccillo@sparkdigital.com>
2018-12-03 17:18:32 -0300

    NGA-6015: Bug fixing.

--- src/render/kekule.render.base.js
+++ src/render/kekule.render.base.js
@@ -1545,11 +1545,12 @@ Kekule.Render.AbstractRenderer = Class.create(ObjectEx,
 	/** @private */
 	createLineBoundInfo: function(coord1, coord2, width, isElectron)
 	{
+		var increasingBondHitArea = 24;
 		if (!isElectron) {
-			var increasingBondHitArea = 24;
 			return this.createBoundInfo(Kekule.Render.BoundShapeType.LINE, [coord1, coord2], {'width': increasingBondHitArea});
 		} else {
-			return this.createBoundInfo(Kekule.Render.BoundShapeType.LINE, [coord1, coord2], {'width': width});
+			increasingBondHitArea = 8;
+			return this.createBoundInfo(Kekule.Render.BoundShapeType.LINE, [coord1, coord2], {'width': increasingBondHitArea});
 		}
 
 	},
