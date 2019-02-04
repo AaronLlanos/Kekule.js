71cc8554fffebe4aa5d75900d61dff47fb5078ae
Medikonda <kranthi.medikonda.contractor@macmillan.com>
2019-02-04 15:54:54 -0600

    addressing comments

--- src/localization/en/kekule.localize.widget.en.js
+++ src/localization/en/kekule.localize.widget.en.js
@@ -20,7 +20,7 @@ const reversibleArrowLine = require("../../../public/svg/reversible-arrow-line.s
 const openArrowDoubleLine = require("../../../public/svg/open-arrow-double-line.svg");
 const heatSymbol = require("../../../public/svg/triangle.svg");
 const addSymbol = require("../../../public/svg/plus.svg");
-const Benzene = require("../../../public/svg/benzene-ring.svg");
+const benzeneRing = require("../../../public/svg/benzene-ring.svg");
 const FlexRing = require("../../../public/svg/n-ring.svg");
 const cycloPropane = require("../../../public/svg/cyclo-propane.svg");
 const cycloButane = require("../../../public/svg/cyclo-butane.svg");
@@ -247,7 +247,7 @@ Kekule.Localization.addResource('en', 'ChemWidgetTexts', {
 	CAPTION_REPOSITORY_RING_6: cycloHexane,
 	CAPTION_REPOSITORY_RING_7: 'Cycloheptane',
 	CAPTION_REPOSITORY_RING_8: 'Cyclooctane',
-	CAPTION_REPOSITORY_RING_AR_6: Benzene,
+	CAPTION_REPOSITORY_RING_AR_6: benzeneRing,
 	CAPTION_REPOSITORY_RING_AR_5: 'Cyclopentadiene',
 	CAPTION_REPOSITORY_CYCLOHEXANE_CHAIR1: 'Cyclohexane chair 1',
 	CAPTION_REPOSITORY_CYCLOHEXANE_CHAIR2: 'Cyclohexane chair 2',
