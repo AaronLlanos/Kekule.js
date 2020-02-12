103c72dee9a0832abfbaf91a115be566f88d4ac1
partridgejiang <partridge@cpu.edu.cn>
2020-02-12 12:19:33 +0800

    Avoid save unneed indirect coord data when serializing ChemObject
    (cherry picked from commit 5a7fe2f5c4e351b75924e7ecc98df17e623b7589)

--- src/core/kekule.common.js
+++ src/core/kekule.common.js
@@ -1748,7 +1748,7 @@ Kekule.ClassDefineUtils = {
 			ClassEx.defineProp(aClass, 'indirectCoordStorage', {
 				//'scope': Class.PropertyScope.PUBLIC,
 				'dataType': DataType.HASH,
-				'serializable': true,
+				'serializable': function(){ return !!this.getEnableIndirectCoord(); },
 				'setter': null,
 				'getter': function()
 				{
