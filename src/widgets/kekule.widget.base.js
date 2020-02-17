614cc1126d9e3285e92883cf67bb36b379501d1f
partridgejiang <partridge@cpu.edu.cn>
2020-02-17 19:00:59 +0800

    Adding a missing semicolon
    (cherry picked from commit 5fa0ef02c9de1a26dd8f7970e880313d81355101)

--- src/widgets/kekule.widget.base.js
+++ src/widgets/kekule.widget.base.js
@@ -1260,7 +1260,7 @@ Kekule.Widget.BaseWidget = Class.create(ObjectEx,
 	{
 		var parent = this.getParent();
 		if (this.getBubbleUiEvents())
-			return true
+			return true;
 		else if (this.getInheritBubbleUiEvents() && parent && parent.getActualBubbleUiEvents)
 			return parent.getActualBubbleUiEvents();
 		else
