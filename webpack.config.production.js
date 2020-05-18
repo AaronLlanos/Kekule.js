0f5095fb64b947e944e6da73412d6fc1dc11b3b0
Miguel Martinez <miguel@miguelmartinez.com>
2020-05-18 17:24:04 -0500

    new build with uglify plugin enabled.

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -45,6 +45,11 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: {
+        except: ['$super']
+      }
     })
   ]
 };
