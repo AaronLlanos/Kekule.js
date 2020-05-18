db4a738c67532f99768d836bd221c6a9bf0a6098
Miguel Martinez <miguel@miguelmartinez.com>
2020-05-18 17:17:15 -0500

    More adjusts to make it compatible with slapi and nodejs

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -45,11 +45,6 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: {
-        except: ['$super']
-      }
     })
   ]
 };
